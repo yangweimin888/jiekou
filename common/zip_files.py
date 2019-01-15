7f39ac676f34c210302294526e48e90d3a14887a
yangweimin888 <1148479571@qq.com>
2019-01-15 10:33:55 +0800

    修复压缩文件时重复的问题

--- common/zip_files.py
+++ common/zip_files.py
@@ -4,6 +4,7 @@
 # 工具: PyCharm
 import zipfile
 import os
+from common.logger import Log
 
 class ZipMethod(object):
     """压缩文件的公共方法"""
@@ -11,7 +12,7 @@ class ZipMethod(object):
 
     def __init__(self, file_path):
         self.file_path = file_path
-        # self.log = Log()
+        self.log = Log()
 
 
     def zip_files(self, zip_name):
@@ -19,24 +20,24 @@ class ZipMethod(object):
         data = os.listdir(self.file_path)
         zip_file = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
         for file in data:
-            print('%s正在压缩中......' % file)
-            zip_file.write(file)
+            self.log.info('%s正在压缩中......' % file)
+            zip_file.write(os.path.join(self.file_path, file))
         zip_file.close()
-        print('压缩完成')
+        self.log.info('压缩完成')
 
 
-    def zip_path(self, zip_name):
-        """按照全路径压缩文件"""
+    def zip_path(self, zip_name, zip_file):
+        """指定具体文件按照全路径进行压缩"""
         data = os.listdir(self.file_path)
         if len(data) == None or len(data) < 1:
-            print('待压缩的文件目录：%s' % self.file_path + '里面不存在文件，不需要压缩')
+            self.log.warning('待压缩的文件目录：%s' % self.file_path + '里面不存在文件，不需要压缩')
         else:
-            zip_file = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
-            for path, dir_names, file_names in os.walk(self.file_path):
-                for filename in file_names:
-                    zip_file.write(os.path.join(self.file_path, filename))
-            zip_file.close()
+            zip = zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED)
+            if zip_file not in data:
+                self.log.info('您所要压缩的文件不存在，请再次确认！')
+            else:
+                zip.write(os.path.join(self.file_path, zip_file))
+                self.log.info('压缩完成')
+            zip.close()
 
 
-app = ZipMethod('E:\Code\python3\day01')
-app.zip_path('esss.zip')
